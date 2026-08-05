feaee1573abfba0f9f94cd6af0869fbd042c5c2b
gillianline <gillian.line@yahoo.com>
2026-08-05 13:03:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import streamlit as st
 import streamlit.components.v1 as components
 import requests
-from streamlit_gsheets import GSheetsConnection
+
 
 def save_to_secret_sheet(athlete, metric, count_val):
     sheet_url = st.secrets.get("Live Track") or st.secrets.get("sheets", {}).get("live_track_url")
